aa53634724f782b211bb0d4f8b0f196dff5dac53
bogdanmironov <bogdanmironov8@gmail.com>
2022-01-14 22:13:46 +0200

    Half of task1

--- IP_21-22/homework_2/playground.cpp
+++ IP_21-22/homework_2/playground.cpp
@@ -4,6 +4,18 @@
 #include <cstring>
 #include <limits>
 
+const char punct[] = {',', '.', '-', '?', '!', ';', ':', '\0'}; // "..."
+
+bool isPunctoation(char c)
+{
+    for (size_t i = 0; i < strlen(punct); ++i) {
+        if (punct[i] == c) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
 
 }
--- IP_21-22/homework_2/task1.cpp
+++ IP_21-22/homework_2/task1.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
+#include <cstring>
+
+const int MAX_SIZE_DICT = 128;
+const int MAX_SIZE_DICT_ENTRY = 100;
+const int MAX_SIZE_INPUT_TEXT = 1024;
+const int MAX_SIZE_OUTPUT_TEXT = 2048;
+
+
+
+bool whitespaceAfterPunc(char);
+bool whitespaceBeforePunc(char);
+//is Quote
+void autoCorrect(char[MAX_SIZE_INPUT_TEXT], char[MAX_SIZE_DICT][MAX_SIZE_DICT_ENTRY]);
 
 int main() {
     
+}
+
+void autoCorrect(char text[MAX_SIZE_INPUT_TEXT], char dict[MAX_SIZE_DICT][MAX_SIZE_DICT_ENTRY]) {
+    char dictA[MAX_SIZE_DICT][MAX_SIZE_DICT_ENTRY/2];
+    char dictB[MAX_SIZE_DICT][MAX_SIZE_DICT_ENTRY/2];
+    char out[MAX_SIZE_OUTPUT_TEXT];
+
+    for (size_t i = 0; strcmp(dict[0], ); ++i) {
+
+    }
+
+    for (size_t i = 0; i < strlen(text); ++i) {
+        if (text[i]) {
+            return ;
+        }
+    }
+
+}
+
+bool whitespaceAfterPunc(char c) {
+    const char afterPunc[] = {',', '.', '-', '?', '!', ';', ':', '+', '-', '*', '/', ']', '}', ')', '>', '\0'}; // "..."
+
+    for (size_t i = 0; i < strlen(afterPunc); ++i) {
+        if (afterPunc[i] == c) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool whitespaceBeforePunc(char c) {
+    const char beforePunc[] = {'-', '+', '-', '*', '/', '[', '{', '(', '<', '\0'}; 
+
+    for (size_t i = 0; i < strlen(beforePunc); ++i) {
+        if (beforePunc[i] == c) {
+            return true;
+        }
+    }
+    return false;
 }
