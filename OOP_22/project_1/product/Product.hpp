df42036b85c9e678398862ee88b8346e7039cad4
bogdanmironov <bogdanmironov8@gmail.com>
2022-05-15 13:45:15 +0300

    Added warehouse classa and created blueprint for Product class

---
+++ OOP_22/project_1/product/Product.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+class Product {
+    char *name = nullptr;
+    char *maufacturer = nullptr;
+    char *comment = nullptr;
+    int expiryDate = 0; //TODO Make date
+    int admissionDate = 0; //TODO Make date
+    int availableQuantity = 0;
+    int placement; //TODO Make class
+};
---
+++ OOP_22/project_1/warehouse/Warehouse.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+class Warehouse {
+public:
+    Warehouse() = default;
+    ~Warehouse();
+};
