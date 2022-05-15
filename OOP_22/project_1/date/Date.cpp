1056fb75150b9964647f303c7a7b4507dadfe523
bogdanmironov <bogdanmironov8@gmail.com>
2022-05-15 19:55:20 +0300

    Added date constructors

--- OOP_22/project_1/date/Date.cpp
+++ OOP_22/project_1/date/Date.cpp
@@ -2,3 +2,41 @@
 #include "Date.hpp"
 #include <iostream>
 
+Date::Date() : Date(1970, 0, 0, 0, 0, 0) {}
+
+Date::Date(size_t year, size_t month, size_t day, size_t hour, size_t min, size_t second) {
+    setYear(year);
+    setMonth(month);
+    setDay(day);
+    setHour(hour);
+    setMin(min);
+    setSecond(second);
+}
+
+Date::Date(size_t year, size_t month, size_t day) {
+    setYear(year);
+    setMonth(month);
+    setDay(day);
+    setHour(0);
+    setMin(0);
+    setSecond(0);
+}
+
+Date::Date(size_t timestamp) {
+    setYear((timestamp / (365 * 30 * 24 * 60 * 60)) + 1970);
+    timestamp %= (365 * 30 * 24 * 60 * 60);
+
+    setMonth(timestamp / (30 * 24 * 60 * 60));
+    timestamp %= (30 * 24 * 60 * 60);
+
+    setDay(timestamp / (24 * 60 * 60));
+    timestamp %= (24 * 60 * 60);
+
+    setHour(timestamp / (60 * 60));
+	timestamp %= (60 * 60);
+
+	setMin(timestamp / 60);
+	timestamp %= 60;
+
+	setSecond(timestamp);
+}
--- OOP_22/project_1/date/Date.hpp
+++ OOP_22/project_1/date/Date.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <iostream>
 
+//Dates are in UTC
+//Months have 30 days
+//Years have 365 days
 class Date {
     size_t year;
     size_t month;
@@ -12,7 +15,7 @@ public:
     Date();
     Date(size_t year, size_t month, size_t day, size_t hour, size_t min, size_t second);
     Date(size_t year, size_t month, size_t day);
-    Date(size_t seconds);
+    Date(size_t timestamp);
 
     size_t getYear() const;
     size_t getMonth() const;
@@ -21,12 +24,12 @@ public:
     size_t getMin() const;
     size_t getSecond() const;
 
-    void setYear();
-    void setMonth();
-    void setDay();
-    void setHour();
-    void setMin();
-    void setSecond();
+    void setYear(size_t year);
+    void setMonth(size_t month);
+    void setDay(size_t day);
+    void setHour(size_t hour);
+    void setMin(size_t min);
+    void setSecond(size_t second);
 
     int compare(const Date& other) const;
 
