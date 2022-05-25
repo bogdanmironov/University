b4c1764113bc53d4fb1510370c1420d3084386f1
bogdanmironov <bogdanmironov8@gmail.com>
2022-05-25 13:25:46 +0300

    Constructor with default values

--- OOP_22/prep/test_25-05-2022/big_4/constructor.cpp
+++ OOP_22/prep/test_25-05-2022/big_4/constructor.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
 class Construct {
-    int a, b;
+    int a, b, c;
 public:
-    Construct() {
-        a = 10;
-        b = 20;
-    }
+    // Construct() {
+    //     a = 10;
+    //     b = 20;
+    // }
 
-    Construct(int a, int b) {
-        setA(a);
-        setB(b);
-    }
+    // Construct(int a, int b) {
+    //     setA(a);
+    //     setB(b);
+    // }
+
+    // This takes care of the above constructors
+    Construct(int a = 1, int b = 2, int c = 3);
 
 
     int getA() const {
@@ -22,6 +25,10 @@ public:
         return b;
     }
 
+    int getC() const {
+        return c;
+    }
+
     void setA(int a) {
         this->a = a;
     }
@@ -29,14 +36,26 @@ public:
     void setB(int b) {
         this->b = b;
     }
+    
+    void setC(int c) {
+        this->c = c;
+    }
 
 };
 
+Construct::Construct(int a, int b, int c) {
+    setA(a);
+    setB(b);
+    setC(c);
+}
+
 
 int main () {
-    Construct c1, c2(30, 40);
+    Construct c1, c2(30, 40), c3(50, 60, 70);
 
-    std::cout << "1:   a: " << c1.getA() << " | b: " << c1.getB() << std::endl;
+    std::cout << "1:   a: " << c1.getA() << " | b: " << c1.getB() << " | c: " << c1.getC() << std::endl;
 
-    std::cout << "2:   a: " << c2.getA() << " | b: " << c2.getB() << std::endl;
+    std::cout << "2:   a: " << c2.getA() << " | b: " << c2.getB() << " | c: " << c2.getC() << std::endl;
+
+    std::cout << "3:   a: " << c3.getA() << " | b: " << c3.getB() << " | c: " << c3.getC() << std::endl;
 }
